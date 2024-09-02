2bf11a6a8e44a6dcebd7cb9b2b015c71f6879e57
Brandon Noronha <brandonnoronha7+github@gmail.com>
2024-09-02 14:36:04 +0530

    learned php and bootstrap

---
+++ PHP/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Hello world!</p>
+    <?php
+    $one=1; //integer
+    $text="I am a string";
+        echo "here's the test code";
+        echo "<table border='1'><tr>";
+        echo "<td>i am one:</td><td>".($one+1)."</td></tr>";
+        echo "<tr><td>i am a string:</td><td>".($text)."</td></tr>";
+        echo "</table>";
+    ?>
+</body>
+</html>
