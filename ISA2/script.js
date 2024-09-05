ce674043940658b425672ff101a868356f3fbf4f
Brandon Noronha <brandonnoronha7+github@gmail.com>
2024-09-05 23:49:20 +0530

    final quiz with explaination file

--- ISA2/script.js
+++ ISA2/script.js
@@ -1,15 +1,20 @@
-var optionsSelected=[];
-var actualAnswers=["&lt; img &gt;","background-image: url('');",""]
-let totalOptions=document.querySelectorAll("input");
-let totalAnswers=document.querySelectorAll("label");
-var score=0;
-function answerCheck(){
-    
+var optionsSelected = [];
+var actualAnswers = ["&lt; img &gt;", "background-image: url('');", "transform: rotate();", "&lt; h1 &gt;", "/* This is a comment. */"]
+let totalOptions = document.querySelectorAll("input");
+let totalAnswers = document.querySelectorAll("label");
+var score = 0;
+
+function answerCheck() {
+    collectSelected();
+    checkScore();
+    // console.log(score);
+    document.getElementById("result").innerHTML = `You have scored ${score} out of 5`;
 }
 
+
 function collectSelected() {
-    for(var i=0;i<totalOptions.length;i++){
-        if(totalOptions[i].checked){
+    for (var i = 0; i < totalOptions.length; i++) {
+        if (totalOptions[i].checked) {
             optionsSelected.push(totalAnswers[i].innerHTML);
         }
     }
@@ -17,6 +22,8 @@ function collectSelected() {
 
 function checkScore() {
     for (let j = 0; j < actualAnswers.length; j++) {
-        
+        if (optionsSelected[j] == actualAnswers[j]) {
+            score++;
+        }
     }
 }
